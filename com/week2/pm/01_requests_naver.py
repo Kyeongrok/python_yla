d534ff14235ddcf8db96a0b8dc0980476d48e986
yeongseon <yeongseon.choe@gmail.com>
2019-03-09 08:41:54 +0900

    3월 9일 교육 자료 - requests 라이브러리 사용법 - html 기본 설명

---
+++ com/week2/pm/01_requests_naver.py
@@ -0,0 +1,15 @@
+import requests
+
+# Pycharm -> Preferences -> Project
+# Create virtual environment
+# Install request package
+
+
+URL = 'http://www.naver.com'
+print(URL)
+
+response = requests.get(URL)
+
+print(response.status_code)
+
+print(response.text)
---
+++ com/week2/pm/02_requests_daum.py
@@ -0,0 +1,10 @@
+import requests
+
+URL = 'http://www.daum.net'
+print(URL)
+
+response = requests.get(URL)
+
+print(response.status_code)
+
+print(response.text)
