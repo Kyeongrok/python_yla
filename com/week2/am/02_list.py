598ac03bae4dfff0d452140e9f85119f123e589b
kyeongrok <oceanfog1@gmail.com>
2019-03-09 11:59:47 +0900

    2번째 시간 list, dict

---
+++ com/week2/am/02_list.py
@@ -0,0 +1,7 @@
+list = []   # 대괄호
+list2 = [1, 2, 3, 4]
+
+print(list2)
+print(list2[0])
+print(list2[3])
+print(list2[2])
---
+++ com/week2/am/05_dictionary.py
@@ -0,0 +1,5 @@
+# dictionary 딕셔너리
+user = {"name":"kyeongrok", "age":33, "birth":87,
+        "job":"developer", "salary":10000000 * 10}
+print(user)
+print(user['salary'])
